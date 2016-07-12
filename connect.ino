437c9762c524029dd5df3797b7cf4bb59137d906
vandaipham <daipham93@gmail.com>
2016-07-12 17:14:44 +0300

    arduino-uno-stend-remote-lampa
    remote lampa via server by arduino-uno and ethernet

--- connect.ino
+++ connect.ino
@@ -8,8 +8,10 @@ byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 byte server[] = { 13, 92, 80, 101 }; // измени на свой
 
 int const ledPin = 8;
+int const congTac = 9;
 
 EthernetClient client;
+EthernetClient client2;
 
 void printIPAddress()
 {
@@ -41,16 +43,28 @@ void setup() {
   Serial.println(Ethernet.localIP());
 
   pinMode(ledPin, OUTPUT); // configure LED-PIN.
+  pinMode(congTac, OUTPUT); // configure cong-tac
 
    //Connect to server, port 4000.
   if(client.connect(server, 4000)) {
-    Serial.print("connected");
+    Serial.print("Client 1 connected");
   }
   else {
-    Serial.println("Connection failed");
+    Serial.println("Client 1 connection failed");
   }
   
-  client.write("A28F26"); // send data to server
+  client.write("8888"); // send data to server
+
+   //Connect to server, port 4000.
+  if(client2.connect(server, 4000)) {
+    Serial.print("Client 2 connected");
+  }
+  else {
+    Serial.println("Client 2 connection failed");
+  }
+  
+  client.write("9999"); // send data to server
+  
 }
 
 void loop() {
@@ -61,9 +75,11 @@ void loop() {
       case 49:  // if char c = 1
         digitalWrite(ledPin, HIGH);
         Serial.println("LED ON");
+        break;
       case 48:  // if char c = 0
         digitalWrite(ledPin, LOW);
         Serial.println("LED OFF");
+        break;
     }
   }
 
@@ -74,10 +90,40 @@ void loop() {
     //for(;;)
     //;
     Serial.println("Connecting....................");
-    if(client.connect(server,80)){
-      Serial.println("Connected");
+    if(client.connect(server,4000)){
+      Serial.println("Client 1 connected");
     }
     else
-      Serial.println("Connection failed");
+      Serial.println("Client 1 connection failed");
+  }
+
+  //for CLIENT 2
+  if (client2.available()) {
+    char c = client2.read();
+    Serial.print(c);
+    switch (c){
+      case 49:  // if char c = 1
+        digitalWrite(congTac, HIGH);
+        Serial.println("CONG TAC ON");
+        break;
+      case 48:  // if char c = 0
+        digitalWrite(congTac, LOW);
+        Serial.println("CONG TAC OFF");
+        break;
+    }
+  }
+
+  if (!client2.connected()) {
+    Serial.println();
+    //Serial.println("disconnecting.");
+    //client.stop();
+    //for(;;)
+    //;
+    Serial.println("Connecting....................");
+    if(client2.connect(server,4000)){
+      Serial.println("Client 2 connected");
+    }
+    else
+      Serial.println("Client 2 connection failed");
   }
 }
