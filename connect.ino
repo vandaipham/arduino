670c0f245eb5836cc438bfb294d3f65de087fa49
vandaipham <daipham93@gmail.com>
2016-07-13 14:49:42 +0300

    Update connect.ino

--- connect.ino
+++ connect.ino
@@ -1,129 +1,252 @@
-#include <SPI.h>
-#include <Ethernet.h>
+#include <ESP8266WiFi.h>
+#include <WiFiUDP.h>
+#include <PubSubClient.h>
+//#include <DHT.h>
+//#include "rgb_lcd.h"
 
-// MAC-адрес нашего устройства 
-byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };    
+//rgb_lcd lcd;
+/*
+const int colorR = 255;
+const int colorG = 0;
+const int colorB = 255;
+*/
+/*/ Variable for Sensors
+#define AirPin A0
+#define DhtPin D3
+float h, t, airValue;
+//Variable
+DHT dht(DhtPin, DHT11, 15);*/
 
-// ip-адрес удалённого сервера 
-byte server[] = { 13, 92, 80, 101 }; // измени на свой
+//varible for wifi
+const char* ssid = "ASUS";
+const char* password = "1234567890";
 
-int const ledPin = 8;
-int const congTac = 9;
+//variable for server
+const char* server = "192.168.1.237";
 
-EthernetClient client;
-EthernetClient client2;
+//Type command
+int cmd;
 
-void printIPAddress()
-{
-  Serial.print("My IP address: ");
-  for (byte thisByte = 0; thisByte < 4; thisByte++) {
-    // print the value of each byte of the IP address:
-    Serial.print(Ethernet.localIP()[thisByte], DEC);
-    Serial.print(".");
+//variable for UDP
+unsigned int localPort = 8888;
+unsigned int portUDP = 10001;
+boolean connectedUDP = false;
+WiFiUDP UDP1;
+WiFiUDP UDP2;
+
+boolean udpConnected = false;
+char packetBuffer[1]; //buffer to hold incoming packet
+
+//variable for TCP
+WiFiClient clientTCP;
+unsigned int portTCP = 10002;
+
+//variable for MQTT
+unsigned int portMQTT = 1883;
+const char* server_mqtt = "test.mosquitto.org";
+WiFiClient espClient;
+PubSubClient client(espClient);
+char msg[50];
+
+//for Random String
+static const char alphanum[] =
+  "0123456789"
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  "abcdefghijklmnopqrstuvwxyz";  // chars we need for generation
+
+int stringLength = sizeof(alphanum) - 1;
+
+char genRandom(){
+
+    return alphanum[rand() % stringLength];
+}
+
+String randomString(int length){
+  String str;
+  for (int i = 0; i < length; i++){
+    str += genRandom();
   }
-  Serial.println();
+  return str;
 }
 
 void setup() {
-  // Open serial communications and wait for port to open:
-  Serial.begin(9600);
-  // this check is only needed on the Leonardo:
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
-  // start the Ethernet connection:
-  if (Ethernet.begin(mac) == 0) {
-    Serial.println("Failed to configure Ethernet using DHCP");
-    // no point in carrying on, so do nothing forevermore:
-    for (;;)
-      ;
-  }
+  Serial.begin(115200);
+  connectWiFi();
+  pinMode(D4, OUTPUT);
+
+  connectUDP();
+
+  client.setServer(server, 1883);
+
+/*  dht.begin(); //start sensor
+  lcd.begin(16,2);
+  lcd.setRGB(colorR, colorG, colorB);*/
   delay(1000);
-  // print your local IP address:
-  Serial.println(Ethernet.localIP());
-
-  pinMode(ledPin, OUTPUT); // configure LED-PIN.
-  pinMode(congTac, OUTPUT); // configure cong-tac
-
-   //Connect to server, port 4000.
-  if(client.connect(server, 4000)) {
-    Serial.print("Client 1 connected");
-  }
-  else {
-    Serial.println("Client 1 connection failed");
-  }
-  
-  client.write("8888"); // send data to server
-
-   //Connect to server, port 4000.
-  if(client2.connect(server, 4000)) {
-    Serial.print("Client 2 connected");
-  }
-  else {
-    Serial.println("Client 2 connection failed");
-  }
-  
-  client.write("9999"); // send data to server
-  
 }
 
 void loop() {
-  if (client.available()) {
-    char c = client.read();
-    Serial.print(c);
-    switch (c){
-      case 49:  // if char c = 1
-        digitalWrite(ledPin, HIGH);
-        Serial.println("LED ON");
-        break;
-      case 48:  // if char c = 0
-        digitalWrite(ledPin, LOW);
-        Serial.println("LED OFF");
-        break;
+  /*
+  //read data Air Sensor
+  airValue = analogRead(A0);
+  h = dht.readHumidity();
+  t = dht.readTemperature();
+  lcd.setCursor(0,0);
+  lcd.print("T:"+String(int(t))+"*C" + " Hum:" + String(int(h)) + "%-->");
+  lcd.setCursor(0,1);
+  lcd.print("Air Value: "); lcd.print(airValue); 
+  */
+  
+  if(connectedUDP){
+  
+    int packetSize = UDP1.parsePacket();
+    char value;
+    if(packetSize){
+      Serial.println();
+      Serial.print("Received packet of size ");
+      Serial.println(packetSize);
+      Serial.print("From ");
+      IPAddress remote = UDP1.remoteIP();
+      for (int i =0; i < 4; i++) {
+        Serial.print(remote[i], DEC);
+        if (i < 3) {
+            Serial.print(".");
+        }
+      }
+      Serial.print(", port ");
+      Serial.println(UDP1.remotePort());
+
+      //read the packet UDP into buffer
+      UDP1.read(packetBuffer, 1);
+      Serial.println("Contents: ");
+      value = packetBuffer[0];
+      Serial.print(value);
     }
+    switch(value){
+      case '1':
+        cmd = 1; break;
+      case '2':
+        cmd = 2; break;
+      case '3':
+        cmd = 3; break;
+      case '0':
+        cmd = 0; break;
+    }
+    if (cmd == 1) {
+      Serial.println("Starting send UDP-packet");
+      sendUDP();
+    }
+    if (cmd == 2) {
+      Serial.println("Starting send TCP_packet");
+      sendTCP();
+    }
+    if (cmd == 3) {
+      Serial.println("Starting send MQTT-packet");
+      sendMQTT();
+    }
+    delay(1000);
+  } else connectUDP();
+}
+
+void connectWiFi(){
+  Serial.println(); Serial.println();
+  Serial.print("Connecting to "); Serial.print(ssid);
+  
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
   }
 
-  if (!client.connected()) {
-    Serial.println();
-    //Serial.println("disconnecting.");
-    //client.stop();
-    //for(;;)
-    //;
-    Serial.println("Connecting....................");
-    if(client.connect(server,4000)){
-      Serial.println("Client 1 connected");
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  digitalWrite(D4, LOW);
+}
+ 
+void connectUDP(){
+  if(WiFi.status() == WL_CONNECTED){
+    Serial.println("Connecting... to UDP");
+    if(UDP1.begin(localPort) == 1){
+      Serial.println("Connection successful");
+      connectedUDP = true;
     }
-    else
-      Serial.println("Client 1 connection failed");
+    else {
+      connectedUDP = false;
+      Serial.println("Connection failed");
+    }
   }
+  
+}
 
-  //for CLIENT 2
-  if (client2.available()) {
-    char c = client2.read();
-    Serial.print(c);
-    switch (c){
-      case 49:  // if char c = 1
-        digitalWrite(congTac, HIGH);
-        Serial.println("CONG TAC ON");
-        break;
-      case 48:  // if char c = 0
-        digitalWrite(congTac, LOW);
-        Serial.println("CONG TAC OFF");
-        break;
-    }
+void sendUDP(){
+  char data[50];
+  for (int i = 0; i < 50; i++){
+    data[i] += genRandom();
   }
+  UDP2.beginPacket(server,portUDP);
+  Serial.print("Sending UDP-packet.....^ ");
+  Serial.println(data);
+  UDP2.write(data);
+  UDP2.endPacket();
+  delay(100);
+}
 
-  if (!client2.connected()) {
-    Serial.println();
-    //Serial.println("disconnecting.");
-    //client.stop();
-    //for(;;)
-    //;
-    Serial.println("Connecting....................");
-    if(client2.connect(server,4000)){
-      Serial.println("Client 2 connected");
+void reconnectTCP(){
+  while (!clientTCP.connected()){
+    Serial.println("Connecting... to TCP-server");
+    if (clientTCP.connect(server, portTCP)){
+      delay(1000);
+      Serial.println("Connected to TCP-server");
+    } else {
+      delay(1000);
+      Serial.println("Failed connect to TCP_server");
     }
-    else
-      Serial.println("Client 2 connection failed");
   }
+}
+
+void sendTCP(){
+  if(!clientTCP.connected()){
+    reconnectTCP();
+  }
+  else {
+    clientTCP.print(randomString(50));
+    Serial.print("Sending TCP-packet...:");
+    Serial.println(randomString(50));
+    delay(100);
+  }
+}
+
+void reconnectMQTT() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("node13")) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("outTopic", "hello world");
+      // ... and resubscribe
+      //client.subscribe("inTopic");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 1 seconds");
+      // Wait 1 seconds before retrying
+      delay(1000);
+    }
+  }
+}
+void sendMQTT(){
+  if (!client.connected()){
+    reconnectMQTT();
+  }
+  Serial.print("Publish message: ");
+  char data[50];
+  for (int i = 0; i < 50; i++){
+    data[i] += genRandom();
+  }
+  Serial.println(data);
+  client.publish("Node13", data);
 }
